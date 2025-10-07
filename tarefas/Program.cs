20d34e869a799fab1c0b7e33cad312adbdcf2cc8
angelo559 <matheus.angelo0301@gmail.com>
2025-10-07 00:14:51 +0000

    Aula 06

---
+++ tarefas/Program.cs
@@ -0,0 +1,10 @@
+﻿public class tarefas{
+
+public id int ;
+public nome string {get; set;}
+public descricao string {get; set;}
+public DataCriacao date time {get; set;}
+public status int {get; set;}
+public DataExecucao dateTime {get; set;}
+
+
---
+++ tarefas/operacoes.cs
@@ -0,0 +1,12 @@
+public class Operacoes
+{ 
+    public int Criar(Tarefa tarefa)
+    {
+        return 0;
+    }
+
+    public Tarefa Buscar(int id)
+    { 
+        return null;
+    
+     
